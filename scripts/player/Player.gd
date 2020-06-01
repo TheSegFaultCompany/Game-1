b760d2260116f3e15fe910eebaa303b8d6678d38
Sahibjeet Babbrah <sbabbrah@uoguelph.ca>
2020-05-31 22:02:55 -0400

    Added all the information for the basic movement

---
+++ scripts/player/Player.gd
@@ -0,0 +1,122 @@
+extends KinematicBody2D
+
+signal turnOnShader
+# Declare member variables here.
+const UP = Vector2(0, -1)
+const GRAVITY = 20
+const ACCELERATION = 50
+const MAX_SPEED = 250
+const SNECK_SPEED = 100
+const JUMP_HEIGHT = -600
+const CHAIN_PULL = 25
+
+# The vector for the chain velocity
+var chain_velocity := Vector2(0, 0)
+
+# Export variables for toggling abilities
+export var grappling = false
+export var anti_gravity = false
+
+# Motion vector for movement of the player object
+var motion = Vector2()
+
+# Function for the click event, use the grappling hook
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.pressed and grappling == true:
+			$chain.shoot(event.position - get_viewport().size * 0.5)
+		else:
+			$chain.release()
+
+# Process where all the movement calculations are done
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("turn_on_shader"):
+		emit_signal("turnOnShader")
+	
+	# Parameter for friction
+	var friction = false
+
+	# Used to be calculating the
+	var walk = (Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")) * MAX_SPEED
+	
+	# Falling Up or Down, depending on the orientation of the gravity
+	if (anti_gravity):
+		motion.y -= GRAVITY
+	else:
+		motion.y += GRAVITY
+	
+	# Hook Physics
+	if $chain.hooked and grappling == true:
+		chain_velocity = to_local($chain.tip).normalized() * CHAIN_PULL
+		if chain_velocity.y > 0:
+			#chain_velocity.y *= 0.55
+			chain_velocity.y *= 0.95
+			pass
+		else:
+			chain_velocity.y *= 1.65
+		if sign(chain_velocity.x) != sign(walk):
+			chain_velocity.x *= 0.7
+			pass
+	else:
+		chain_velocity = Vector2(0, 0)
+	# Add the grappling motion to the player motion
+	motion += chain_velocity
+	
+	# Moving the character right
+	if Input.is_action_pressed("ui_right"):
+		if Input.is_action_pressed("ui_down"):
+			motion.x = min(motion.x + ACCELERATION, SNECK_SPEED)
+		else:
+			motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		$AnimatedSprite.play("Walking (right)")
+	# Moving the character left
+	elif Input.is_action_pressed("ui_left"):
+		if Input.is_action_pressed("ui_down"):
+			motion.x = max(motion.x - ACCELERATION, -SNECK_SPEED)
+		else:
+			motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		$AnimatedSprite.play("default")
+	# Idle animation played otherwise
+	else:
+		$AnimatedSprite.play("default")
+		friction = true
+	# Shifting the character into a crouching position
+	# When shifting the character cannot use the grappling hook
+	if Input.is_action_pressed("ui_down"):
+		$CollisionStanding.disabled = true
+		$CollisionCrouching.disabled = false
+		grappling = false
+	else:
+		$CollisionStanding.disabled = false
+		$CollisionCrouching.disabled = true
+		grappling = true
+	
+	#If the player is on the floor, then initiate the jump or
+	# Slow down the movement if no input is pressed
+	if is_on_floor() and anti_gravity == false:
+		_normalJumping(friction)
+	elif is_on_ceiling() and anti_gravity == true:
+		_upsideDownJumping(friction)
+	else:
+		#If the character is moving up, then play the up animation
+		if motion.y < 0:
+			#$AnimatedSprite.play("Jumping")
+			pass
+		else:
+			if motion.x > 0:
+				$AnimatedSprite.play("Walking (right)")
+			pass
+	motion = move_and_slide(motion, UP)
+
+func _upsideDownJumping(friction: bool) -> void:
+	if Input.is_action_just_pressed("ui_up"):
+		motion.y = -JUMP_HEIGHT
+	if friction == true:
+		motion.x = lerp(motion.x, 0, 0.2)
+	pass
+
+func _normalJumping(friction: bool) -> void:
+	if Input.is_action_just_pressed("ui_up"):
+		motion.y = JUMP_HEIGHT
+	if friction == true:
+		motion.x = lerp(motion.x, 0, 0.2)
---
+++ scripts/player/chain.gd
@@ -0,0 +1,41 @@
+extends Node2D
+
+# To make sure the hook can interact with tilemap, make sure the second unit
+# in the collision layer and collision mask is activated as well.
+onready var links = $links
+var direction := Vector2(0, 0)
+var tip := Vector2(0, 0)
+
+const SPEED = 50
+
+var flying = false
+var hooked = false
+
+func shoot(dir: Vector2) -> void:
+	direction = dir.normalized()
+	flying = true
+	tip = self.global_position
+
+func release() -> void:
+	flying = false
+	hooked = false
+
+func _process(delta: float) -> void:
+	self.visible = flying or hooked
+	if not self.visible:
+		return
+	var tip_loc = to_local(tip)
+	links.rotation = self.position.angle_to_point(tip_loc) - deg2rad(90)
+	$Tip.rotation = self.position.angle_to_point(tip_loc) - deg2rad(90)
+	links.position = tip_loc
+	links.region_rect.size.y = tip_loc.length()
+	if tip_loc.length() > 1000:
+		release()
+
+func _physics_process(delta: float) -> void:
+	$Tip.global_position = tip
+	if flying:
+		if $Tip.move_and_collide(direction * SPEED):
+			hooked = true
+			flying = false
+	tip = $Tip.global_position
